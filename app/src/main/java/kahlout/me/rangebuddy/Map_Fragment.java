fed302a7311b22810c9f29853a318d1cfad3cddb
malikahlout <mali@kahlout.me>
2018-06-24 20:14:02 +0100

    Basic maps working

--- app/src/main/java/kahlout/me/rangebuddy/Map_Fragment.java
+++ app/src/main/java/kahlout/me/rangebuddy/Map_Fragment.java
@@ -1,24 +1,38 @@
 package kahlout.me.rangebuddy;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
-    GoogleMap mMap;
+    private GoogleMap mMap;
+    private boolean mLocationPermissionGranted;
 
     @Nullable
     @Override
@@ -26,13 +40,12 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
 
         View view = inflater.inflate(R.layout.fragment_map, null, false);
-
-
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
 
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
 
         return view;
 
@@ -41,15 +54,109 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap){
-//        mMap = googleMap;
+    public void onMapReady(GoogleMap googleMap) {
 
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        updateLocationUI();
+        mMap = googleMap;
 
+        mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);
+        enableMyLocationIfPermitted();
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMinZoomPreference(11);
+
+//        // Add a marker in Sydney, Australia,
+//        // and move the map's camera to the same location.
+//        LatLng sydney = new LatLng(-33.852, 151.211);
+//        mMap.addMarker(new MarkerOptions().position(sydney)
+//                .title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//
+//        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//
+//
+//            @Override
+//            public void onMapClick(LatLng clickCoords){
+//
+//                Toast.makeText(getActivity(), "Map Click",
+//                        Toast.LENGTH_SHORT).show();
+//
+//
+//
     }
+
+
+
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{
+
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    1);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void showDefaultLocation() {
+        Toast.makeText(getActivity(), "Location permission not granted, " +
+                        "showing default location",
+                Toast.LENGTH_SHORT).show();
+        LatLng redmond = new LatLng(47.6739881, -122.121512);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(redmond));
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocationIfPermitted();
+                    mLocationPermissionGranted = true;
+                } else {
+                    showDefaultLocation();
+                    mLocationPermissionGranted = false;
+                }
+                return;
+            }
+
+        }
+    }
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener =
+            new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    mMap.setMinZoomPreference(15);
+                    return false;
+                }
+            };
+
+
+    @SuppressWarnings("MissingPermission")
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+
+        if (mLocationPermissionGranted) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            mMap.setMyLocationEnabled(false);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+
+
 }
